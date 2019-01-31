9869f32f5c280305187d160fcc5734c4d58e99cd
isuruAb <vpisabeywardana@gmail.com>
2019-01-31 19:46:11 +0800

    avoid duplicate entries

--- src/App.js
+++ src/App.js
@@ -21,21 +21,34 @@ class App extends Component {
   ];
 
   finalWord = [];
+  finalIndex = [];
   neigbour_arr = [];
   selectChar(e, params) {
     console.log(e.target.id);
     console.log(params);
+    let selectedIndex = e.target.id.split("");
 
+    for (var i = 0; i < selectedIndex.length; i++) {
+      selectedIndex[i] = parseInt(selectedIndex[i], 10);
+    }
 
     let row_limit = this.board.length;
     let column_limit = this.board.length;
+    
+    //Check Existance
+    if(this.finalIndex.length!==0){
+      for(let index=0;index<this.finalIndex.length;index++){
+        if(this.finalIndex[index][0]===selectedIndex[0]&&this.finalIndex[index][1]===selectedIndex[1]){
+          alert("You can't select same character again");
+          return;
+        }
+      }
+    }
+
     if (this.neigbour_arr.length !== 0) {
       let isNeigbour = false;
-      let selectedIndex = e.target.id.split("");
 
-      for (var i = 0; i < selectedIndex.length; i++) {
-        selectedIndex[i] = parseInt(selectedIndex[i], 10);
-      }
+
       for (let neigbour = 0; neigbour < this.neigbour_arr.length; neigbour++) {
         if (selectedIndex[0] === this.neigbour_arr[neigbour][0] && selectedIndex[1] === this.neigbour_arr[neigbour][1]) {
           console.log('They are equal!', JSON.stringify(this.neigbour_arr[neigbour]));
@@ -48,7 +61,8 @@ class App extends Component {
         return;
       }
       else {
-        this.finalWord.push(params)
+        this.finalWord.push(params);
+        this.finalIndex.push([selectedIndex[0],selectedIndex[1]]);
         document.getElementById(e.target.id).style.background = '#345678';
 
         this.neigbour_arr = [];
@@ -66,16 +80,13 @@ class App extends Component {
       }
     }
     else {
-      this.finalWord.push(params)
+      this.finalWord.push(params);
+      this.finalIndex.push([selectedIndex[0],selectedIndex[1]]);
+
       document.getElementById(e.target.id).style.background = '#345678';
 
       console.log('no length else');
       this.neigbour_arr = [];
-      let selectedIndex = e.target.id.split("");
-
-      for (let i = 0; i < selectedIndex.length; i++) {
-        selectedIndex[i] = parseInt(selectedIndex[i], 10);
-      }
 
       if (row_limit > 0) {
         for (let x = Math.max(0, selectedIndex[1] - 1); x <= Math.min(selectedIndex[1] + 1, row_limit - 1); x++) {
@@ -89,10 +100,10 @@ class App extends Component {
       }
     }
 
-
-
-
     console.log("this.finalWord", this.finalWord);
+    
+    console.log("this.finalIndex", this.finalIndex);
+
 
   }
   render() {
