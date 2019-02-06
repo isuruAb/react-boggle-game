a345856f09f465e6e8691d7111b7e182d9dad5f1
isuruAb <vpisabeywardana@gmail.com>
2019-02-06 16:36:25 +0800

    add page not found screen

---
+++ src/components/notfound/notfound.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+import './notfound.css'
+
+class NotFound extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="NotFound">
+          <div className="banner">
+            <h1>404 Page Not Found</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
