ce39aa9e7a375f8472ccd2f7b5888d65a799b944
isuruAb <vpisabeywardana@gmail.com>
2019-03-21 14:58:11 +0800

    redirect un authenticated users

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function PrivateRoute ({component: Component, authenticated, ...rest}) {
       {...rest}
       render={(props) => localStorage.getItem("token")
         ? <Component {...props} />
-        : <Redirect to={{pathname: '/game', state: {from: props.location}}} />}
+        : <Redirect to={{pathname: '/', state: {from: props.location}}} />}
     />
   )
 }
